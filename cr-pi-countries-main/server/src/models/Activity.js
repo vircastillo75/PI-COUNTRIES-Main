10657506c15bcae33167561d24c290efbe743ac2
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2023-10-08 08:13:11 -0300

    first commit

---
+++ cr-pi-countries-main/server/src/models/Activity.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Activity', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        // allowNull: false,
+        autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    difficulty: {
+        type: DataTypes.ENUM('1', '2', '3', '4', '5'), //son strings
+        allowNull: false
+    },
+    duration:{
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
+    season: {
+        type: DataTypes.ENUM('Summer', 'Autumn', 'Winter', 'Spring'),
+        allowNull: false,
+    }
+  },
+  { timestamps: false });
+};
--- cr-pi-countries-main/server/src/models/Country.js
+++ cr-pi-countries-main/server/src/models/Country.js
@@ -4,9 +4,36 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('Country', {
+    id: {
+      type: DataTypes.STRING(3),
+      primaryKey:true,
+      // allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
-};
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    subregion: {
+      type: DataTypes.STRING,
+    },
+    area: {
+      type: DataTypes.FLOAT,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {timestamps: false});
+};  
